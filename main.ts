a694c0ba86c77f0e8721d844bb913d8747ae00fc
HunterSLind <38479490+HunterSLind@users.noreply.github.com>
2026-07-03 11:49:57 -0700

    Pencil init (#3)
    * Write to piece of paper
    
    * step back
    
    * added append functionality
    
    * added pencil degredation
    
    * refactored
    
    * added degredation tests and refactored
    
    * write multiple spaces now
    
    * commented test
    
    * added degredation per character
    
    * degrade more when capital letters are given

--- main.ts
+++ main.ts
@@ -1,5 +1,53 @@
+export let paper: string = "";
+
+let defaultDurability = 13;
+let durability: number = defaultDurability;
+
+const isAllUpperCase = (str: string): boolean => {
+  return str === str.toUpperCase();
+};
+
+export function write(text: string) {
+    for(let i : number = 0; i < text.length; i++ ){
+        let letterToWrite = text[i];
+        
+        if (durability > 0) {
+            paper += letterToWrite;
+        }
+        else {
+            paper += " ";
+        }
+        if(letterToWrite != " "){
+            if(isAllUpperCase(letterToWrite)){
+                durability -= 2
+            }
+            else{
+                durability -= 1;
+            }
+            
+        }
+        
+    }
+}
 
 
-export function helloWorld(): boolean {
-    return true;
+
+export function getDurability(): number {
+    return durability;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export function clear() {
+    paper = "";
+    durability = defaultDurability;
 }
