81e357d193df4f19e6339c2a321b6f107897fb3d
Hunter Lind <hunterslind@gmail.com>
2026-07-03 13:03:42 -0500

    added pencil degredation

--- main.test.ts
+++ main.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { write, paper, clear } from './main'
+import { write, paper, clear, getDurability } from './main'
 
 
 beforeEach(() => {
@@ -9,14 +9,26 @@ beforeEach(() => {
 
 describe('writing to paper', () => {
   it('Writes to paper', () => {
-    write('Hello, world!')
-    expect(paper).toBe('Hello, world!')
+    write('Hello, world!');
+    expect(paper).toBe('Hello, world!');
   });
 
   it('Always appends to paper', () => {
-    write('Hello, world')
-    expect(paper).toBe('Hello, world')
-    write('!')
-    expect(paper).toBe('Hello, world!')
+    write('Hello, world');
+    expect(paper).toBe('Hello, world');
+    write('!');
+    expect(paper).toBe('Hello, world!');
+  });
+});
+
+describe('writing causes degredation', () => {
+  it('degrades pencil by 1 when writing an "e"', () => {
+    write('e');
+    expect(getDurability()).toBe(9);
+  });
+
+  it('degrades pencil by 2 when writing an "ee"', () => {
+    write('ee');
+    expect(getDurability()).toBe(8);
   });
 });
--- main.ts
+++ main.ts
@@ -1,10 +1,34 @@
 export let paper: string = "";
 
+let durability: number = 10;
+
 
 export function write(text: string) {
     paper += text;
+    for (let i: number = 0; i < text.length; i++) {
+        durability -= 1
+    }
+    
 }
+
+
+
+export function getDurability(): number {
+    return durability;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 export function clear() {
     paper = "";
+    durability = 10;
 }
