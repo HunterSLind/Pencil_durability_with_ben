3a182a73f7115304545fa09a2add88d367e97979
Hunter Lind <hunterslind@gmail.com>
2026-07-03 12:15:46 -0500

    initialized project, added test, added github action workflow yml

---
+++ main.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+import { helloWorld } from './main'
+
+
+test('greets the user correctly', () => {
+  expect(helloWorld()).toBe(true)
+})
---
+++ main.ts
@@ -0,0 +1,5 @@
+
+
+export function helloWorld(): boolean {
+    return true;
+}
